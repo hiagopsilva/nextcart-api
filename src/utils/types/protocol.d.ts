876dd5ff6d624280f2b43ad576524a4482411e42
Hiago Prates <hiagohps360@gmail.com>
2022-08-28 13:54:46 -0300

    feat: types

---
+++ src/utils/types/protocol.d.ts
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+declare namespace Protocol {
+  declare type Item = {
+    protocolo: string,
+    apresentante: string,
+    tipo: string,
+    imagem: string,
+    entrada: number,
+    vencimento: number
+  };
+
+  declare type List = {
+    id: string,
+    protocolo: string,
+    apresentante: string,
+    tipo: string,
+    imagem: string,
+    entrada: number,
+    vencimento: number
+  };
+}
